bc04370a592b352c01926391063976d115fa7d77
Matt Adams <mattaadams1@gmail.com>
2020-10-15 20:32:53 -0400

    latest version

--- amptorch/active_learning/ensemble_calc.py
+++ amptorch/active_learning/ensemble_calc.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from ase.calculators.calculator import Calculator
 from amptorch.utils import make_amp_descriptors_simple_nn
-
+import pandas as pd
 
 __author__ = "Muhammed Shuaibi"
 __email__ = "mshuaibi@andrew.cmu.edu"
@@ -29,12 +29,31 @@ class EnsembleCalc(Calculator):
         self.trained_calcs = trained_calcs
         self.training_params = training_params
 
-    def calculate_stats(self, energies, forces):
+    median_list = [100]
+    statistics = []
+    def calculate_stats(self, energies, forces,energy_list = median_list,stat_list = statistics):
         median_idx = np.argsort(energies)[len(energies) // 2]
         energy_median = energies[median_idx]
-        energy_var = np.var(energies)
+        prev_e_mean = energy_list[-1] #previous median energy
+        energy_list.append(energy_median)
+        #print('previous median',prev_e_mean)
+        stat_list.append([np.max(np.var(forces,  axis=0)),np.max(np.std(forces, axis=0))])
         forces_median = forces[median_idx]
+        l = []
+        def fmax(forces):
+            for i in range(len(forces)):
+                l.append(np.max(np.absolute(forces[i])))
+            return l
+           
+        fmax = fmax(forces)
+        #print('fmax_predict',fmax[median_idx])
+
+        #print(np.max(forces))
         max_forces_var = np.max(np.var(forces, axis=0))
+        #print('forces',forces)
+        #print(max_forces_var)
+        df = pd.DataFrame(stat_list,columns = ['max force var','max force stdev'])
+        df.to_csv('energy_force_statistics.csv',index=False)
         return energy_median, forces_median, max_forces_var
 
     def calculate(self, atoms, properties, system_changes):
@@ -67,3 +86,4 @@ def make_fps(images, Gs):
     make_amp_descriptors_simple_nn(
         images, Gs, elements, cores=1, label="ensemble"
     )
+
--- amptorch/active_learning/oal_calc.py
+++ amptorch/active_learning/oal_calc.py
@@ -1,7 +1,9 @@
 import os
 import sys
 import copy
-
+import numpy as np
+import pandas as pd
+from ase.db import connect
 from ase.calculators.singlepoint import SinglePointCalculator as sp
 from ase.calculators.calculator import Calculator
 
@@ -47,29 +49,43 @@ class AMPOnlineCalc(Calculator):
 
         self.uncertain_tol = training_params["uncertain_tol"]
         self.parent_calls = 0
-
-    def calculate(self, atoms, properties, system_changes):
+    median_list = [1e8,1e7]
+    e_diffs = []
+    def calculate(self, atoms, properties, system_changes,energy_list=median_list,e_diffs=e_diffs):
         Calculator.calculate(self, atoms, properties, system_changes)
 
         energy_pred = self.ensemble_calc.get_potential_energy(atoms)
         force_pred = self.ensemble_calc.get_forces(atoms)
+        fmax_pred = np.max(np.absolute(force_pred))
+        #print('ensemb_pred_fmax',fmax_pred)
         uncertainty = atoms.info["uncertainty"][0]
-
+        db = connect('dft_calls.db')
+        new_data = atoms.copy()
+        new_data.set_calculator(copy.copy(self.parent_calc))
+        energy_actual = new_data.get_potential_energy(apply_constraint=False)
+        fmax_actual = np.max(np.absolute(new_data.get_forces(apply_constraint=False)))
+        #print('fmax_actual',fmax_actual)
+        e_diffs.append([energy_pred,energy_actual,(energy_actual-energy_pred),fmax_pred,fmax_actual,(fmax_actual-fmax_pred),uncertainty])
+        df1 = pd.DataFrame(e_diffs,columns=['predicted energy','actual energy','delta','predicted force','actual force','delta','uncertainty'])
+        df1.to_csv('Actual_vs_Predicted.csv',index=False)
         cwd = os.getcwd()
-        if uncertainty >= self.uncertain_tol:
+        if uncertainty >= self.uncertain_tol: #or np.absolute(energy_pred-energy_list[-1]) >= np.absolute(energy_list[-1]-energy_list[-2]):
+            print('DFT required')
             new_data = atoms.copy()
             new_data.set_calculator(copy.copy(self.parent_calc))
-            os.makedirs("./temp", exist_ok=True)
-            os.chdir("./temp")
+           # os.makedirs("./temp", exist_ok=True)
+           # os.chdir("./temp")
 
             energy_pred = new_data.get_potential_energy(apply_constraint=False)
             force_pred = new_data.get_forces(apply_constraint=False)
             new_data.set_calculator(
                 sp(atoms=new_data, energy=energy_pred, forces=force_pred)
             )
-            os.chdir(cwd)
-            os.system("rm -rf ./temp")
+           # os.chdir(cwd)
+           # os.system("rm -rf ./temp")
 
+            energy_list.append(energy_pred)
+            db.write(new_data)
             self.ensemble_sets, self.parent_dataset = bootstrap_ensemble(
                 self.parent_dataset, self.ensemble_sets, new_data=new_data
             )
@@ -78,6 +94,9 @@ class AMPOnlineCalc(Calculator):
                 self.ensemble_sets, self.training_params, self.n_cores
             )
             self.parent_calls += 1
-
+        else:
+            db.write(None)
+            energy_list.append(energy_pred)
+      #  print('energy_list',energy_list)
         self.results["energy"] = energy_pred
         self.results["forces"] = force_pred
